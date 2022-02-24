097f55351b8e67fde13a866f1ab5144dbd6ae9f0
Mick Hansen <maker@mhansen.io>
2022-02-24 15:30:54 +0100

    rest of getting started pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,5 +12,41 @@ exports.createPages = ({ actions }) => {
     isPermanent: true, 
     force: true
   });
+  createRedirect({
+    fromPath: "/authentication/*", 
+    toPath: "/verify/authentication/:splat", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/authentication", 
+    toPath: "/verify/authentication", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/eid-specifics/*", 
+    toPath: "/verify/eid-specifics/:splat", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/eid-specifics", 
+    toPath: "/verify/eid-specifics", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/how-to/*", 
+    toPath: "/verify/how-to/:splat", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/how-to", 
+    toPath: "/verify/how-to", 
+    isPermanent: true, 
+    force: true
+  });
 };
 
