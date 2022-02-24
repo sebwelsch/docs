37a61d42391fb17911f4367a72a25d3fdbde5edc
Mick Hansen <maker@mhansen.io>
2022-02-24 15:00:37 +0100

    tweak redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,14 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
   createRedirect({
-    fromPath: "/getting-started*", 
-    toPath: "/verify/getting-started:splat", 
+    fromPath: "/getting-started/*", 
+    toPath: "/verify/getting-started/:splat", 
+    isPermanent: true, 
+    force: true
+  });
+  createRedirect({
+    fromPath: "/getting-started", 
+    toPath: "/verify/getting-started", 
     isPermanent: true, 
     force: true
   });
