d392245623808e4b9ec1475b6af3966c8463c108
Mick Hansen <maker@mhansen.io>
2022-02-24 16:15:47 +0100

    algolia indexing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://new-docs-test.criipto.com",
@@ -20,6 +22,15 @@ module.exports = {
         ignore: [`**/*\.graphql\.tsx`],
       },
       __key: "pages",
-    },
-  ],
+    }
+  ].concat(process.env.GATSBY_ALGOLIA_APP_ID ? [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries")
+      },
+    }
+  ] : []),
 };
---
+++ src/utils/algolia-queries.js
@@ -0,0 +1,38 @@
+// const escapeStringRegexp = require("escape-string-regexp")
+// const pagePath = `src/pages`
+const indexName = `test`
+const pageQuery = `{
+  pages: allMdx {
+    edges {
+      node {
+        id
+        frontmatter {
+          product
+          title
+          category
+          subtitle
+        }
+        slug
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+function pageToAlgoliaRecord({ node: { id, frontmatter, fields, ...rest } }) {
+  return {
+    objectID: id,
+    ...frontmatter,
+    ...fields,
+    ...rest,
+    _tags: (frontmatter.product ? [frontmatter.product] : []).concat(frontmatter.category ? [frontmatter.category] : [])
+  }
+}
+const queries = [
+  {
+    query: pageQuery,
+    transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+    indexName,
+    settings: { attributesToSnippet: [`excerpt:20`] },
+  },
+]
+module.exports = queries
