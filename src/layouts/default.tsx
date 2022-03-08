59ac6988e5a5ebc16f2eea38683b2fb102adcb28
Mick Hansen <maker@mhansen.io>
2022-03-08 13:21:12 +0100

    react-helmet data

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 import {DesktopNavigation} from '../components/Navigation';
 import Header from '../components/Header';
 
-export default function DefaultLayout(props: {children: React.ReactNode, pageContext: any, path: string}) {
+export default function DefaultLayout(props: {children: React.ReactNode, pageContext: any, path: string, pageResources: any}) {
   const {frontmatter} = props.pageContext;
+  const description = frontmatter.description || frontmatter.subtitle;
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{frontmatter.title}{frontmatter.title === 'Criipto Documentation' ? null : ' - Criipto Documentation'}</title>
+        <link rel="canonical" href={`https://new-docs-test.criipto.com${props.pageResources?.page?.path}`} />
+        {description && (<meta name="description" content={description} />)}
+      </Helmet>
+
       <Header path={props.path} />
       <div className="px-4 sm:px-6 md:px-8">
         <div className="max-w-screen-2xl mx-auto pt-10 lg:pl-[19.5rem] xl:pr-[19.5rem]">
