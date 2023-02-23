721e2076c7ad933833d6d06d1a7e0998f03d6fbe
Mick Hansen <maker@mhansen.io>
2023-02-23 18:36:19 +0100

    Fix webhook example typo

--- src/examples/createSignatureOrder.graphql.tsx
+++ src/examples/createSignatureOrder.graphql.tsx
@@ -138,3 +138,12 @@ export const uniqueEvidenceExampleVariables = () : {input: CreateSignatureOrderI
     }
   };
 }
+
+export const webhookExampleVariables = () : {input: CreateSignatureOrderInput} => {
+  return {
+    input: {
+      webhook: {url: 'https://httpbin.org/post'},
+      ...variables().input,
+    }
+  };
+}
