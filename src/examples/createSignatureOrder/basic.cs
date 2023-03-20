27e5f4d9cf2636c7a43c6083ec11690842369ec5
Mick Hansen <maker@mhansen.io>
2023-03-20 21:03:25 +0100

    add basic csharp example with compiler check

---
+++ src/examples/createSignatureOrder/basic.cs
@@ -0,0 +1,25 @@
+using Criipto.Signatures;
+using Criipto.Signatures.Models;
+
+using (var client = new CriiptoSignaturesClient("{YOUR_CRIIPTO_CLIENT_ID}", "{YOUR_CRIIPTO_CLIENT_SECRET}")) {
+    // Setup document input
+    var documents = new List<DocumentInput>{
+        new DocumentInput {
+            pdf = new PadesDocumentInput {
+                title = "Dotnet Sample",
+                blob = new Byte[64], // Should be the bytes of a PDF file
+                storageMode = DocumentStorageMode.Temporary
+            }
+        }
+    };
+
+    // Setup signature order input
+    var createSignatureOrderInput = new CreateSignatureOrderInput
+    {
+        title = "Dotnet Sample",
+        documents = documents
+    };
+
+    // Create signature order
+    var signatureOrder = await client.CreateSignatureOrder(createSignatureOrderInput);
+}
