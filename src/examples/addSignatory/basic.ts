01bbe30f9d69917912fd90b2725eb5f5be79cee9
Mick Hansen <maker@mhansen.io>
2023-05-24 13:44:09 +0200

    fix import error

--- src/examples/addSignatory/basic.ts
+++ src/examples/addSignatory/basic.ts
@@ -1,5 +1,5 @@
 import csharp from '!!raw-loader!./basic.cs';
-import nodejs from '!!raw-loader!./basic.node.cs';
+import nodejs from '!!raw-loader!./basic.node.ts';
 import { Example } from '../misc';
 import { query, variables } from './basic.graphql';
 
