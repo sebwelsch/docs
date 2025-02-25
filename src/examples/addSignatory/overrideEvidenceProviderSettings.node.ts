ea5c309b5423dfbb51df17a272794337746f3cb2
Lasse Hay-Schmidt <36488481+haysch@users.noreply.github.com>
2025-02-25 09:57:26 +0100

    Add docs for overriding signatory EP settings
    Create section for overriding evidence provider settings per signatory
    and add examples.

---
+++ src/examples/addSignatory/overrideEvidenceProviderSettings.node.ts
@@ -0,0 +1,47 @@
+import CriiptoSignatures from "@criipto/signatures";
+
+const client = new CriiptoSignatures(
+  "{YOUR_CRIIPTO_CLIENT_ID}",
+  "{YOUR_CRIIPTO_CLIENT_SECRET}"
+);
+
+// Create signature order
+const signatureOrder = await client.createSignatureOrder({
+  title: "Node.js Sample",
+  documents: [
+    {
+      pdf: {
+        title: "Node.js Sample",
+        blob: Buffer.from("..."), // Should be the buffer of a PDF file
+        storageMode: "Temporary",
+      },
+    },
+  ],
+});
+
+const criiptoVerifyEvidenceProviderId = signatureOrder.evidenceProviders.find(
+  (ep) => ep.__typename === "CriiptoVerifySignatureEvidenceProvider"
+);
+
+if (!criiptoVerifyEvidenceProviderId)
+  throw new Error("Criipto Verify Evidence Provider not found");
+
+// Add signatory
+var signatory = await client.addSignatory(signatureOrder.id, {
+  evidenceProviders: [
+    {
+      id: criiptoVerifyEvidenceProviderId.id,
+      criiptoVerify: {
+        acrValues: [
+          "urn:grn:authn:dk:mitid:substantial",
+          "urn:grn:authn:se:bankid:another-device:qr",
+          "urn:grn:authn:no:bankid:substantial",
+        ],
+        message: "signatory message",
+        loginHint: "signatory login hint",
+      },
+    },
+  ],
+});
+
+console.log(signatory.href); // Signing link, redirect user to this link, or send it in an email
