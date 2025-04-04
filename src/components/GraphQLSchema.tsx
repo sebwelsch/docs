58f693ad6c8e5815092118e2da745ff90eaf378e
Mick Hansen <maker@mhansen.io>
2025-04-04 09:42:52 +0200

    update graphiql dependency
    this keeps us up to date, and in the future we can make a more custom
    schema explorer that would allow us to deep-link etc.

--- src/components/GraphQLSchema.tsx
+++ src/components/GraphQLSchema.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import {DocExplorer} from 'graphiql';
+import React, { useMemo } from 'react';
+import {DocExplorer, EditorContextProvider, ExplorerContextProvider, SchemaContextProvider} from '@graphiql/react';
 import {buildClientSchema, IntrospectionQuery} from 'graphql';
 
 import './GraphQLSchema.css'
 import jsonSchema from '../../graphql-signatures-schema.json';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
+
 const schema = buildClientSchema(jsonSchema.data as any as IntrospectionQuery);
 
 interface GraphQLSchemaProps {}
 export default function GraphQLSchemaComponent(props: GraphQLSchemaProps) {
+  const fetcher = useMemo(() => {
+    if (typeof window === 'undefined') return null;
+    return createGraphiQLFetcher({
+      url: 'https://signatures-api.criipto.com/v1/graphql'
+    });
+  }, []);
+
+  if (fetcher === null) return null;
+
   return (
-    <div className="graphiql-container graphql-schema">
-      <DocExplorer schema={schema} />
-    </div>
+    <EditorContextProvider>
+      <SchemaContextProvider
+        fetcher={fetcher}
+        schema={schema}
+      >
+        <ExplorerContextProvider>
+          <div className="graphiql-container graphql-schema" style={{display: 'block'}}>
+            <DocExplorer />
+            {/* <SchemaDocumentation schema={schema} /> */}
+          </div>
+        </ExplorerContextProvider>
+      </SchemaContextProvider>
+    </EditorContextProvider>
   )
 }
