a2b7350052fe00887dbee4ad974cb23b2ff67c5f
Mick Hansen <maker@mhansen.io>
2025-11-20 12:48:51 +0100

    fix redirect domain

--- plugins/static-redirects/gatsby-node.js
+++ plugins/static-redirects/gatsby-node.js
@@ -1,14 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const redirects = [
-  'https://docs.criipto.io/*  https://docs.idura.app/:splat  301!'
+  'https://docs.criipto.com/*  https://docs.idura.app/:splat  301!'
 ];
 
 exports.onPostBuild = ({ reporter, basePath, pathPrefix }) => {
   const redirectsPath = path.resolve(`${__dirname}/../../public/_redirects`);
-  console.log(redirectsPath);
-  console.log(fs.readFileSync(redirectsPath, 'utf-8'));
   const input = fs.readFileSync(redirectsPath, 'utf-8');
   const output = `${input}\n${redirects.join('\n')}`;
+  console.log(output);
   fs.writeFileSync(redirectsPath, output);
 }
